b8be445162b1635b8a59a60f35b26fddc8aad60c
Alejandro Barrachina <alejba02@ucm.es>
2022-02-24 10:17:18 +0100

    Receta de documento y extensiones recomendadas

--- src/build/build_main_document.ps1
+++ src/build/build_main_document.ps1
@@ -1,5 +1,13 @@
-Get-ChildItem -Recurse -Filter ../docs/*.Rtex | Foreach-Object {
+Set-Location ../../
+Get-ChildItem -Recurse -Filter *.Rtex | Foreach-Object {
     $FixedName = $_.FullName.Replace("\", "/")
     $Dirname = $_.FullName.Replace("\", "/").Replace(".Rtex", ".tex")
     C:/"Program Files"/R/R-4.1.2/bin/x64/R.exe -e "library(knitr);knit2pdf('$FixedName', '$Dirname')" 
 }
+Set-Location docs
+xelatex.exe -file-line-error -interaction=nonstopmode Main.tex
+# makeglossaries.exe docs/Main.Tex 
+xelatex.exe -file-line-error -interaction=nonstopmode Main.tex
+xelatex.exe -file-line-error -interaction=nonstopmode Main.tex 
+Set-Location ..
+Set-Location src/build
