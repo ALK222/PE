78a040a0bf775a47e2dfe9a4f544c61be151ad02
Alejandro Barrachina <alejba02@ucm.es>
2022-02-24 10:44:11 +0100

    prueba de pipeline

--- src/build/build_main_document.ps1
+++ src/build/build_main_document.ps1
@@ -1,13 +1,26 @@
+if ($args.Length -gt 1) {
+    throw "Número incorrecto de argumentos, ponga compile para compilar el documento o  no ponga nada."
+}
+
+compile = $args[0]
+
+if (compile -ne "compile") {
+    throw "Argumento no reconocido, ponga compile para compilar el documento o  no ponga nada."
+}
+
 Set-Location ../../
 Get-ChildItem -Recurse -Filter *.Rtex | Foreach-Object {
     $FixedName = $_.FullName.Replace("\", "/")
     $Dirname = $_.FullName.Replace("\", "/").Replace(".Rtex", ".tex")
     C:/"Program Files"/R/R-4.1.2/bin/x64/R.exe -e "library(knitr);knit2pdf('$FixedName', '$Dirname')" 
 }
-Set-Location docs
-xelatex.exe -file-line-error -interaction=nonstopmode Main.tex
-# makeglossaries.exe docs/Main.Tex 
-xelatex.exe -file-line-error -interaction=nonstopmode Main.tex
-xelatex.exe -file-line-error -interaction=nonstopmode Main.tex 
-Set-Location ..
+
+if (compile -eq compile) {
+    Set-Location docs
+    xelatex.exe -file-line-error -interaction=nonstopmode Main.tex
+    makeglossaries.exe docs/Main 
+    xelatex.exe -file-line-error -interaction=nonstopmode Main.tex
+    xelatex.exe -file-line-error -interaction=nonstopmode Main.tex 
+    Set-Location ..
+}
 Set-Location src/build
